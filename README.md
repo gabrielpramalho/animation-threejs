e3073ff00e2544cff004dc22c24cad39ab7d1f8d
GABRIEL PEDRO RAMALHO <biel-pedro@hotmail.com>
2022-09-07 14:04:58 -0300

    background

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# animation-threejs
+# Animação com Three.Js
+
+### Trabalho Final da matéria de Computação Gráfica da Universidade Estadual do Norte do Paraná
+
+## Especificações
+
+- Utilizar animação com RequestAnimationFrame
+- Utilize pelo menos 3 tipos diferentes de geometrias
+- Utilize pelo menos 2 tipos de materiais
+- Carregue pelo menos 1 textura
+- Possua pelo menos 2 fontes de iluminação
+- Carregue pelo menos um modelo externo
+- Realize a criação de objetos dinâmicos
+- Possua algum tipo de interação com o usuário (mouse ou teclado)
--- main.js
+++ main.js
@@ -6,7 +6,7 @@ import { GUI } from './threejs/examples/jsm/libs/lil-gui.module.min.js';
 
 let mixer, modelReady, stats;
 var obj, animations;
-
+var walk, idle;
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.01, 1000);
@@ -29,12 +29,15 @@ loader.load("./threejs/examples/models/gltf/Soldier.glb",
             if ( object.isMesh ) object.castShadow = true;
 
         } );
+
+        obj.scale.set(2, 2, 2);
         
         animations = gltf.animations
         mixer = new THREE.AnimationMixer(obj);
 
-        let action = mixer.clipAction( animations[2] ).play();
-        console.log(action)
+        walk = mixer.clipAction( animations[3] )
+        idle = mixer.clipAction( animations[0] ).play();
+        
 
         
         scene.add(gltf.scene)
@@ -54,7 +57,12 @@ loader.load("./threejs/examples/models/gltf/Soldier.glb",
         
     });
 
-scene.background = new THREE.Color(0x202020)
+const backgroundTexture = new THREE.TextureLoader().load('./assets/stars-2.jpg');
+backgroundTexture.wrapS = THREE.RepeatWrapping;
+backgroundTexture.wrapT = THREE.RepeatWrapping;
+backgroundTexture.repeat.set( 2, 2 );
+
+scene.background = backgroundTexture;
 
 var light = new THREE.HemisphereLight(0xffffff, 0x000000, 5);
 scene.add(light)
@@ -69,7 +77,7 @@ camera.position.set(-5, 1.8, 0);
 
 const clock = new THREE.Clock()
 
-const geometry = new THREE.PlaneGeometry(50, 50 );
+const planeGeometry = new THREE.PlaneGeometry(50, 50 );
 
 const PlaneTexture = new THREE.TextureLoader().load( './assets/floor_pebbles/textures/floor_pebbles_01_diff_4k.jpg' );
 PlaneTexture.wrapS = THREE.MirroredRepeatWrapping
@@ -77,47 +85,43 @@ PlaneTexture.wrapT = THREE.MirroredRepeatWrapping
 PlaneTexture.repeat.set( 30, 30 );
 
 
-const material = new THREE.MeshBasicMaterial({map: PlaneTexture, side: THREE.DoubleSide});
-const plane = new THREE.Mesh(geometry, material);
+const planeMaterial = new THREE.MeshBasicMaterial({map: PlaneTexture, side: THREE.DoubleSide});
+const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -0.5 * Math.PI;
 plane.position.set(0, 0, 0);
 
 scene.add(plane);
 
 
-const cubeGeometry = new THREE.BoxGeometry( 2, 2, 2 );
-const cubeMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-const cube = new THREE.Mesh( cubeGeometry, cubeMaterial );
-scene.add( cube );
-cube.position.set(10, 0, 10)
-
-const capsuleGeometry = new THREE.CapsuleGeometry( 1, 1, 4, 8 );
-const capsuleMaterial = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-const capsule = new THREE.Mesh( capsuleGeometry, capsuleMaterial );
-scene.add( capsule );
-
-capsule.position.set(10, 1.5, -10)
-
-
-const coneGeometry = new THREE.ConeGeometry( 2, 10, 12 );
-const coneMaterial = new THREE.MeshBasicMaterial( {color: 0xffff00} );
-const cone = new THREE.Mesh( coneGeometry, coneMaterial );
-const cone2 = new THREE.Mesh( coneGeometry, coneMaterial );
-scene.add( cone );
-
-cone.position.set(20, 0, 0)
-
-
 let controls = {
     rotationSpeed: 0.02,
     numberOfObjects: scene.children.length,
-    removeCube: function () {
+    removeObj: function () {
         var allChildren = scene.children;
         var lastObject = allChildren[allChildren.length - 1];
         if (lastObject instanceof THREE.Mesh) {
             scene.remove(lastObject);
             this.numberOfObjects = scene.children.length;
         }
+    },
+
+    addCone: function () {
+
+        var coneSize = Math.ceil((Math.random() * 3));
+        var coneGeometry = new THREE.ConeGeometry(coneSize, coneSize + 5, coneSize + 7);
+        var coneMaterial = new THREE.MeshToonMaterial({
+            color: Math.random() * 0xffffff
+        });
+        var cone = new THREE.Mesh(coneGeometry, coneMaterial);
+        cone.castShadow = true;
+        cone.name = "cone-" + scene.children.length;
+
+        cone.position.x = +30 + Math.round((Math.random() * planeGeometry.parameters.width));
+        cone.position.y = 0
+        cone.position.z = -20 + Math.round((Math.random() * planeGeometry.parameters.height));
+        scene.add(cone);
+        this.numberOfObjects = scene.children.length;
+        console.log('Created cone with name: ' + cone.name);
     },
 
     addCube: function () {
@@ -131,9 +135,9 @@ let controls = {
         cube.castShadow = true;
         cube.name = "cube-" + scene.children.length;
 
-        cube.position.x = -30 + Math.round((Math.random() * plane.parameters.width));
-        cube.position.y = Math.round((Math.random() * 5));
-        cube.position.z = -20 + Math.round((Math.random() * plane.parameters.height));
+        cube.position.x = +30 + Math.round((Math.random() * planeGeometry.parameters.width));
+        cube.position.y = 0
+        cube.position.z = -20 + Math.round((Math.random() * planeGeometry.parameters.height));
         scene.add(cube);
         this.numberOfObjects = scene.children.length;
         console.log('Created cube with name: ' + cube.name);
@@ -143,7 +147,8 @@ let controls = {
 var gui = new GUI();
 // gui.add(controls, 'rotationSpeed', 0, 0.5);
 gui.add(controls, 'addCube');
-gui.add(controls, 'removeCube');
+gui.add(controls, 'addCone');
+gui.add(controls, 'removeObj');
 gui.add(controls, 'numberOfObjects').listen();
 
 
@@ -164,38 +169,40 @@ animate()
 document.addEventListener('keydown', (event) => {
     
     if(event.key == "w"){
-        obj.position.x += 0.02
+        obj.position.x += 0.1
         obj.rotation.y = -1.6
         animamationModel("walk")
         mixer.update(clock.getDelta())
     } else if(event.key == "s"){
-        obj.position.x -= 0.02
+        obj.position.x -= 0.1
         obj.rotation.y = 1.6
         animamationModel("walk")
     } else if(event.key == "a"){
-        obj.position.z -= 0.02
+        obj.position.z -= 0.1
         obj.rotation.y = 0
         animamationModel("walk")
     } else if(event.key == "d"){
-        obj.position.z += 0.02
+        obj.position.z += 0.1
         obj.rotation.y = -3.2
         animamationModel("walk")
     }
 
 })
 
-// document.addEventListener('keyup', (event) => {
-//     if(event.key){
-//         animamationModel("idle")
-//     }
-// })
+document.addEventListener('keyup', (event) => {
+    if(event.key){
+        animamationModel("idle")
+    }
+})
 
 
 function animamationModel(action){
     if(action == "walk"){
-        mixer.clipAction( animations[3] ).play();
+        idle.stop()
+        walk.play()
     }else if(action == "idle"){
-        mixer.clipAction( animations[2] ).play();
+        walk.stop()
+        idle.play()
     }
 }
                 
